cf0d3d402c709d86de0346b96fe3ed8b665a779f
samson-adekeye <97424717+samson-adekeye@users.noreply.github.com>
2022-02-28 02:03:46 -0600

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-jan-s3-bucket-samson"
+    bucket = "mys3bucket-jan"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamo-table"
