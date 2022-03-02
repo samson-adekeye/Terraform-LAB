0218eafd68395bb6811f4c4bbf4d03f71af16151
samson-adekeye <97424717+samson-adekeye@users.noreply.github.com>
2022-03-01 22:32:08 -0600

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket"
+    bucket = "newtrial2022"
     key = "main"
-    region = "us-east-2"
-    dynamodb_table = "my-dynamodb-table"
+    region = "us-east-1"
+    dynamodb_table = "my-table-123-march"
   }
 }
